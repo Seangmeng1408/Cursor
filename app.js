9580d581e91ad1cb201f694b84f3dd9c3656aa66
dinseangmeng <dinseangmeng@gmail.com>
2022-10-08 12:40:36 +0700

    add click event

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ window.addEventListener("mousemove",(e)=>{
     focus.style.left=e.clientX-focusWidth+"px";
     focus.style.top=e.clientY-focusWidth+"px";
 })
+window.addEventListener("click",()=>{
+    window.open("https://www.dinseangmeng.xyz");
+})
