82e6d610ae2bb3a18c776e189808e272662d6085
dinseangmeng <dinseangmeng@gmail.com>
2022-10-08 12:37:11 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var focus=document.querySelector(".mouse_on");
+var focusWidth=focus.offsetWidth/2;
+
+window.addEventListener("mousemove",(e)=>{
+    focus.style.left=e.clientX-focusWidth+"px";
+    focus.style.top=e.clientY-focusWidth+"px";
+})
